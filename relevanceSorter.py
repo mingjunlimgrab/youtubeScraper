4a78833c506702fc84ded55b447c326e15e3b84d
Mingjun Lim <mingjun.lim@ITSG000765-MAC.local>
2018-07-03 15:35:37 +0800

    added sorted csv

---
+++ relevanceSorter.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import webbrowser
+import csv
+
+fileToRead = 'zeroTo2k.csv'
+fileToWrite = 'sorted02k.csv'
+
+df = pd.read_csv(fileToRead)
+
+one_published_at = []
+one_video_id = []
+one_title = []
+one_description = []
+one_relevance = []
+
+zero_published_at = []
+zero_video_id = []
+zero_title = []
+zero_description = []
+zero_relevance = []
+
+index = 0
+for index in range(len(df['title'])):
+    if df['relevance'][index] == '1':
+        one_published_at.append(df['published_at'][index])
+        one_video_id.append(df['video_id'][index])
+        one_title.append(df['title'][index])
+        one_description.append(df['description'][index])
+        one_relevance.append(df['relevance'][index])
+    else:
+        zero_published_at.append(df['published_at'][index])
+        zero_video_id.append(df['video_id'][index])
+        zero_title.append(df['title'][index])
+        zero_description.append(df['description'][index])
+        zero_relevance.append(df['relevance'][index])
+
+num_ones = len(one_video_id)
+num_zeros = len(zero_video_id)
+percentYield = (num_ones * 100)/(num_ones + num_zeros)
+
+one_published_at.extend(zero_published_at)
+one_video_id.extend(zero_video_id)
+one_title.extend(zero_title)
+one_description.extend(zero_description)
+one_relevance.extend(zero_relevance)
+
+new_df = pd.DataFrame(one_published_at, columns=['published_at'])
+new_df['video_id'] = one_video_id
+new_df['title'] = one_title
+new_df['description'] = one_description
+new_df['relevance'] = one_relevance
+
+print("total rows added is " + str(len(new_df['title'])))
+print("number of relevant results: " + str(num_ones))
+print("number of irrelevant results: " + str(num_zeros))
+print("percent of relevant videos: " + str(percentYield))
+
+new_df.to_csv((fileToWrite))
+
