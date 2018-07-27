f68cbedcf2bd81fc4eb6cf66a979a905c368590b
mingjun.lim <mingjun.lim@grabtaxi.com>
2018-07-27 16:44:45 +0800

    fulldata

--- Threat/nltkAnalysisThreatPredictor.py
+++ Threat/nltkAnalysisThreatPredictor.py
@@ -25,15 +25,15 @@ new_words = [('grab mod', 100), ('mod grab', 100), ('grabmod', 100)]
 
 deterministic = [' grab mod ', ' mod grab ', 'mod grab ', 'grabmod']
 
-c = open('my_classifier_threat.pickle', 'rb')
-mnb = open('MNB_classifier_threat.pickle', 'rb')
-bnb = open('BernoulliNB_classifier_threat.pickle', 'rb')
-lg = open('LogisticRegression_classifier_threat.pickle', 'rb')
-sgd = open('SGD_classifier_threat.pickle', 'rb')
-lsvc = open('LinearSVC_classifier_threat.pickle', 'rb')
-wf = open('word_features_threat.pickle', 'rb')
-tr = open('train_set_threat.pickle', 'rb')
-te = open('test_set_threat.pickle', 'rb')
+c = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/my_classifier_threat.pickle', 'rb')
+mnb = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/MNB_classifier_threat.pickle', 'rb')
+bnb = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/BernoulliNB_classifier_threat.pickle', 'rb')
+lg = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/LogisticRegression_classifier_threat.pickle', 'rb')
+sgd = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/SGD_classifier_threat.pickle', 'rb')
+lsvc = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/LinearSVC_classifier_threat.pickle', 'rb')
+wf = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/word_features_threat.pickle', 'rb')
+tr = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/train_set_threat.pickle', 'rb')
+te = open('/Users/mingjun.lim/Documents/youtubeScraper/Pickles/test_set_threat.pickle', 'rb')
 
 classifier = pickle.load(c)
 MNB_classifier = pickle.load(mnb)
@@ -157,6 +157,20 @@ def positive_append(titlesWithRelevance, classifier, printt=False):
     false_negatives = [item for item in toprint if item[1] == 'HIG']
     print("Number of False_negatives is: " + str(len(false_negatives)))
 
+def spitter(strink):
+    title = strink
+    title = dehypdeslash(title)
+    if checker(title.lower()):
+        spit = 1
+    else:
+        toke = special_features(title)
+        cleaned = clean(title)
+        for item in toke:
+            cleaned.append(item)
+        d_f = document_features(cleaned)
+        spit = LinearSVC_classifier.classify(d_f)
+    return spit
+
 def checker(title):
     for word in deterministic:
         if word in title:
@@ -179,6 +193,8 @@ def LinearSVC_deterministic_predictor(titlesWithRelevance, classifier):
                 thingy[2] = 'HIG'
             else:
                 thingy.append('HIG')
+            if thingy[1] != thingy[2]:
+                toprint.append(thingy)
             continue
 
         toke = special_features(title)
@@ -200,6 +216,47 @@ def LinearSVC_deterministic_predictor(titlesWithRelevance, classifier):
     false_negatives = [item for item in toprint if item[1] == 'HIG']
     print("Number of False_negatives is: " + str(len(false_negatives)))
     print("Number of False_positives is: " + str(len(toprint) - len(false_negatives)))
+
+def outputSaver(NBclassifierFn, csvTest):
+    df = pd.read_csv(csvTest)
+    index = 0
+    published_at = []
+    video_id = []
+    title = []
+    description = []
+    relevance = []
+    threat = []
+    pred_relevance = []
+    pred_threat = []
+
+    for item in df['title']:
+        titlez = dehypdeslash(item)
+        toke = special_features(titlez)
+        cleaned = clean(titlez)
+        for item in toke:
+            cleaned.append(item)
+        d_f = document_features(cleaned)
+        if NBclassifierFn(d_f) == 'HIG':
+            published_at.append(df['published_at'][index])
+            video_id.append(df['video_id'][index])
+            title.append(df['title'][index])
+            description.append(df['description'][index])
+            relevance.append(df['relevance'][index])
+            threat.append(df['threat'][index])
+            pred_relevance.append(df['pred_relevance'][index])
+            pred_threat.append('HIG')
+        index += 1
+
+    new_df = pd.DataFrame(published_at, columns=['published_at'])
+    new_df['video_id'] = video_id
+    new_df['title'] = title
+    new_df['description'] = description
+    new_df['relevance'] = relevance
+    new_df['threat'] = threat
+    new_df['pred_relevance'] = pred_relevance
+    new_df['pred_threat'] = pred_threat
+
+    new_df.to_csv('/Users/mingjun.lim/Documents/youtubeScraper/Data/secondRound.csv')
 
 def create_test_set(csvFileName): #takes in a string (the name of a file or directory)
     df = pd.read_csv(csvFileName)
@@ -247,31 +304,33 @@ test_set = create_test_set('/Users/mingjun.lim/Documents/youtubeScraper/Data/fir
 # append_truth_predictor(test_set, LinearSVC_classifier)
 # print("voteclassifier accuracy:", (nltk.classify.accuracy(vote_classifier, documents)))
 # append_truth_predictor(test_set, vote_classifier)
-append_truth_predictor(test_set, vote_classifier)
-
-test_set = create_test_set('firstRoundSorted.csv')
-print("Original Naive Bayes accuracy:", (nltk.classify.accuracy(classifier, documents)))
-append_truth_predictor(test_set, classifier)
-# positive_append(test_set, classifier, True)
-print("MNB_classifier accuracy:", (nltk.classify.accuracy(MNB_classifier, documents)))
-append_truth_predictor(test_set, MNB_classifier)
-# positive_append(test_set, MNB_classifier, False)
-print("BernoulliNB_classifier accuracy:", (nltk.classify.accuracy(BernoulliNB_classifier, documents)))
-append_truth_predictor(test_set, BernoulliNB_classifier)
-# positive_append(test_set, BernoulliNB_classifier, False)
-print("LogisticRegression_classifier accuracy:", (nltk.classify.accuracy(LogisticRegression, documents)))
-append_truth_predictor(test_set, LogisticRegression)
-# positive_append(test_set, LogisticRegression, True)
-print("SGD_classifier accuracy:", (nltk.classify.accuracy(SGD_classifier, documents)))
-append_truth_predictor(test_set, SGD_classifier)
-# positive_append(test_set, SGD_classifier, True)
-print("LinearSVC_classifier accuracy:", (nltk.classify.accuracy(LinearSVC_classifier, documents)))
-append_truth_predictor(test_set, LinearSVC_classifier)
-# positive_append(test_set, LinearSVC_classifier, True)
-# print("voteclassifier accuracy:", (nltk.classify.accuracy(vote_classifier, documents)))
 # append_truth_predictor(test_set, vote_classifier)
-# positive_append(test_set, vote_classifier, True)
-LinearSVC_deterministic_predictor(test_set, LinearSVC_classifier)
+#
+# test_set = create_test_set('/Users/mingjun.lim/Documents/youtubeScraper/Data/firstRound.csv')
+# print("Original Naive Bayes accuracy:", (nltk.classify.accuracy(classifier, documents)))
+# append_truth_predictor(test_set, classifier)
+# # positive_append(test_set, classifier, True)
+# print("MNB_classifier accuracy:", (nltk.classify.accuracy(MNB_classifier, documents)))
+# append_truth_predictor(test_set, MNB_classifier)
+# # positive_append(test_set, MNB_classifier, False)
+# print("BernoulliNB_classifier accuracy:", (nltk.classify.accuracy(BernoulliNB_classifier, documents)))
+# append_truth_predictor(test_set, BernoulliNB_classifier)
+# # positive_append(test_set, BernoulliNB_classifier, False)
+# print("LogisticRegression_classifier accuracy:", (nltk.classify.accuracy(LogisticRegression, documents)))
+# append_truth_predictor(test_set, LogisticRegression)
+# # positive_append(test_set, LogisticRegression, True)
+# print("SGD_classifier accuracy:", (nltk.classify.accuracy(SGD_classifier, documents)))
+# append_truth_predictor(test_set, SGD_classifier)
+# # positive_append(test_set, SGD_classifier, True)
+# print("LinearSVC_classifier accuracy:", (nltk.classify.accuracy(LinearSVC_classifier, documents)))
+# append_truth_predictor(test_set, LinearSVC_classifier)
+# # positive_append(test_set, LinearSVC_classifier, True)
+# # print("voteclassifier accuracy:", (nltk.classify.accuracy(vote_classifier, documents)))
+# # append_truth_predictor(test_set, vote_classifier)
+# # positive_append(test_set, vote_classifier, True)
+# LinearSVC_deterministic_predictor(test_set, LinearSVC_classifier)
+
+outputSaver(LinearSVC_classifier.classify, '/Users/mingjun.lim/Documents/youtubeScraper/Data/firstRound.csv')
 
 c.close()
 mnb.close()
